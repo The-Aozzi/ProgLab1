a6e3580ab7f9d02b74c1255d80dd215a0d66dc99
The-Aozzi <theaozzixd@gmail.com>
2024-09-14 12:09:45 +0500

    Commit 2.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! Version 1.0." << VERSION_PATCH << std::endl;
+    std::cout << "Hello, World! Version 1.0." << PATCH_VERSION << std::endl;
     return 0;
 }
